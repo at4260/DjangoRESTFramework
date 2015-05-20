8e76e45682abd12b6e26209bda425c8768e0b772
alicetsao <alice.tsao@brightscope.com>
2015-05-20 10:09:54 -0700

    added permissions and authentication (tutorial 4)

---
+++ tutorial/snippets/migrations/0002_auto_20150520_0935.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('snippets', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='snippet',
+            name='highlighted',
+            field=models.TextField(),
+        ),
+        migrations.AlterField(
+            model_name='snippet',
+            name='owner',
+            field=models.ForeignKey(related_name='snippets', to=settings.AUTH_USER_MODEL),
+        ),
+    ]
